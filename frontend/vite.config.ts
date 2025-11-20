39916fab7be2a025e5ac847cb3bdf93c65739bc2
Zekken26 <kiangutierrez02@gmail.com>
2025-11-20 13:48:19 +0800

    Build for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/quick-serve/",
+  base: mode === "production" ? "/quick-serve/" : "/",
   server: {
     host: "::",
     port: 8080,
